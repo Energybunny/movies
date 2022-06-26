724f902c8ec908a0f5e256dace571ea2d8692a9e
Daniel Wilsson <wilsson.danne@gmail.com>
2022-06-26 16:27:01 +0200

    Updates

---
+++ precache-manifest.76ed7144cbcfb8a18ad6cc316e13712b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1c78382a3c5a44fe6d4",
+    "url": "/movies/static/css/main.0ab91202.chunk.css"
+  },
+  {
+    "revision": "f1c78382a3c5a44fe6d4",
+    "url": "/movies/static/js/main.f1c78382.chunk.js"
+  },
+  {
+    "revision": "97564db6e8cda96558eb",
+    "url": "/movies/static/js/1.97564db6.chunk.js"
+  },
+  {
+    "revision": "6ad4d8afb5a54c964d9f",
+    "url": "/movies/static/js/runtime~main.6ad4d8af.js"
+  },
+  {
+    "revision": "a575752f8b0356e3ec55d2afc4871ea6",
+    "url": "/movies/index.html"
+  }
+];
